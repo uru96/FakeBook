c0752b9feb1dc6424f42d935fb6b3b7b32c0fc09
uru96 <w.uruski96@gmail.com>
2021-11-16 20:16:25 +0100

    register user

--- FakeBook/account/forms.py
+++ FakeBook/account/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 
-# Creating login form view
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    # Using PasswordInput widget to let browser know that is a password
-    password = forms.CharField(widget=forms.PasswordInput)
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password(self):
+        cd = self.cleaned_data
+
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie są identyczne.')
+
+        return cd['password2']
+
--- FakeBook/account/views.py
+++ FakeBook/account/views.py
@@ -2,39 +2,64 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import UserRegistrationForm
 
 # Task for decorator is to check if current user was authenticated
-#
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': dashboard})
 
-# User login view
-def user_login(request):
 
-    if request.method == 'POST':
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
 
-        # Create form with sended user data
-        form = LoginForm(request.POST)
+        if user_form.is_valid():
+            # Creating new user object, without saving in database
+            new_user = user_form.save(commit=False)
 
-        # Check if data in form is valid
-        if form.is_valid():
-            # Authenticate user based on data in database, get username and password and return user object or None
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                # Check if user is active
-                if user.is_active:
-                    # Call login method and start session for user
-                    login(request, user)
-                    return HttpResponse('Uwierzytelnienie zakończone powodzeniem.')
-                else:
-                    return HttpResponse('Konto zablokowane')
-            else:
-                return HttpResponse('Podano nieprawidłowe dane')
+            # Setting new password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+
+            # Saving user object
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
     else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+        user_form = UserRegistrationForm()
+        return render(request, 'account/register',
+                      {'user_form': user_form})
+
+
+
+
+# User login view
+# def user_login(request):
+#
+#     if request.method == 'POST':
+#
+#         # Create form with sended user data
+#         form = LoginForm(request.POST)
+#
+#         # Check if data in form is valid
+#         if form.is_valid():
+#             # Authenticate user based on data in database, get username and password and return user object or None
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 # Check if user is active
+#                 if user.is_active:
+#                     # Call login method and start session for user
+#                     login(request, user)
+#                     return HttpResponse('Uwierzytelnienie zakończone powodzeniem.')
+#                 else:
+#                     return HttpResponse('Konto zablokowane')
+#             else:
+#                 return HttpResponse('Podano nieprawidłowe dane')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
 
