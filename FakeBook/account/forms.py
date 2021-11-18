b4b7fa92a4e4817c307afa04565e4fa4c954fdfa
uru96 <w.uruski96@gmail.com>
2021-11-18 20:41:34 +0100

    user edit form

--- FakeBook/account/forms.py
+++ FakeBook/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserRegistrationForm(forms.ModelForm):
@@ -14,10 +15,28 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
 
-
-
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Hasła nie są identyczne.')
 
         return cd['password2']
 
+    def clean_email(self):
+        cd = self.cleaned_data
+
+        if User.objects.filter(email=cd['email']).exists():
+            print(cd['email'])
+            raise forms.ValidationError('Email już istnieje')
+
+        return cd['email']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
--- FakeBook/account/views.py
+++ FakeBook/account/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-#from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserEditForm, UserProfileEditForm
+from .models import Profile
 
 # Task for decorator is to check if current user was authenticated
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': dashboard})
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = UserProfileEditForm(instance=request.user.profile,
+                                           data=request.POST,
+                                           files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = UserProfileEditForm(instance=request.user.profile)
+
+        return render(request, 'account/edit.html', {'user_form': user_form,
+                                                     'profile_form': profile_form})
 
 
 def register(request):
@@ -19,20 +37,24 @@ def register(request):
             new_user = user_form.save(commit=False)
 
             # Setting new password
-            new_user.set_password(
-                user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data['password'])
 
             # Saving user object
             new_user.save()
+
+            # Create user profile
+            profile = Profile.objects.create(user=new_user)
+
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
+
     else:
         user_form = UserRegistrationForm()
 
     return render(request,
                   'account/register.html',
-                 {'user_form': user_form})
+                  {'user_form': user_form})
 
 
 
